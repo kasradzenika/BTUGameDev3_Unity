5d6a976ff580ae07a0b4d978777c30f17ecadc00
kasradzenika <kasradzenika@yahoo.com>
2017-04-11 20:50:15 +0400

    lecture 3

---
+++ Assets/BTUGameDev3.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BTUGameDev3 : MonoBehaviour {
+
+    public int a = 10;
+    public float b = 4.5656f;
+    public string c = "This is my temp string";
+    public bool d = false;
+    public float startingMaxSpeed = 5f;
+    
+	void Start () {
+
+    }
+	
+	void Update () {
+		
+	}
+}
